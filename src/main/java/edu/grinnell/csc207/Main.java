0385fbafc78b93fc0a259f5833251fd41e83f24f
bakracmi <bakracm@gmail.com>
2024-11-07 21:22:20 -0600

    Last of JavaDocs

--- src/main/java/edu/grinnell/csc207/Main.java
+++ src/main/java/edu/grinnell/csc207/Main.java
@@ -7,7 +7,19 @@ import edu.grinnell.csc207.util.Matrix;
 
 import java.io.PrintWriter;
 
+/**
+ * Main class of our Matrix game.
+ *
+ * @author Harrison Zhu
+ * @author Mina Bakrac
+ */
 public class Main {
+    /**
+     * Display board and pieces.
+     *
+     * @param pen
+     *      Pen for output.
+     */
     public static void display(PrintWriter pen) { 
         Matrix<Character> board = Game.getInstance().getBoard();
         pen.println("Score: " + Game.getInstance().getScore());
@@ -15,14 +27,20 @@ public class Main {
             for(int j = 0; j < board.width(); ++j) { 
                 pen.print(board.get(i, j));
                 pen.print(" ");
-            }
+            } // for
             pen.println();
-        }
-    }
+        } // for
+    } // display(PrintWriter)
+
+    /**
+     * Main method of our game.
+     *
+     * @param args
+     *      Command line arguments.
+     */
     public static void main(String[] args) {
         PrintWriter pen = new PrintWriter(System.out, true);
         Scanner scan = new Scanner(System.in);
-
         //print instruction
         pen.println("""
                 Welcome to our awesome game!
@@ -44,15 +62,13 @@ public class Main {
                 you eat the food, your score grows by one and the food moves to a new
                 spot on the board.
                 """);
-
         Game game = Game.getInstance();
         display(pen);
         while (!KeyboardInput.getInstance().getFullInput().equals("quit") && !game.getGameOver()){//while
             KeyboardInput.getInstance().promptAndRecordInput();
             game.update();
             display(pen);
-        }
-
+        } // while
         pen.println("GAME OVER!");
     } // main(String[])
 } // class Main
--- src/main/java/edu/grinnell/csc207/UI/KeyboardInput.java
+++ src/main/java/edu/grinnell/csc207/UI/KeyboardInput.java
@@ -4,34 +4,52 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
- * Processes user input in the console
+ * Processes user input in the console.
+ *
+ * @author Harrison Zhu
+ * @author Mina Bakrac
  */
 public class KeyboardInput {
     private static KeyboardInput instance = new KeyboardInput();
+
+    /**
+     * Get instance of keyboard input.
+     *
+     * @return instance.
+     */
     public static KeyboardInput getInstance() {
         return instance;
-    }
+    } // getInstance()
 
     private String lastRecordedInput;
     private PrintWriter pen = new PrintWriter(System.out, true);
     private Scanner scan = new Scanner(System.in);
 
+
     private KeyboardInput() { 
         lastRecordedInput = "";
-    }
+    } // KeyboardInput()
 
+    /**
+     * Prompt and record the input.
+     */
     public void promptAndRecordInput() { 
         pen.print(":");
         pen.flush();
         lastRecordedInput = scan.nextLine();
-    }
+    } // promptAndRecordInput()
 
     public Character getInput() {
         if (! this.lastRecordedInput.isEmpty())
             return this.lastRecordedInput.charAt(0);
             return Character.valueOf(' ');
-        }
+    } // getInput()
 
+    /**
+     * Returns full last recorded input.
+     *
+     * @return last recorded input.
+     */
     public String getFullInput() { 
         return this.lastRecordedInput;
     }
