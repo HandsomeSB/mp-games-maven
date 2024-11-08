523184ab53e07509fdbb2b4cfadfff9aed3f5b51
bakracmi <bakracm@gmail.com>
2024-11-07 21:11:55 -0600

    JavaDocs for Vector2D and Game

--- src/main/java/edu/grinnell/csc207/Game.java
+++ src/main/java/edu/grinnell/csc207/Game.java
@@ -11,6 +11,12 @@ import edu.grinnell.csc207.util.MatrixV0;
 import edu.grinnell.csc207.util.Vector2D;
 import java.util.Random;
 
+/**
+ * Create the game. Initialize the pieces and turn logic.
+ *
+ * @author Harrison Zhu
+ * @author Mina Bakrac
+ */
 public class Game {
     // contains board, winning condition, facilitation
     private Matrix<java.lang.Character> board;
@@ -33,19 +39,17 @@ public class Game {
     }
 
     /**
-     * 
+     * Initialize the board and pieces.
      */
     private void init() { 
         //place character
         this.player = new Player(new Vector2D(0, 0),'P', this.board.height(), this.board.width());
-
         this.enemy = new Enemy(new Vector2D(7, 0), 'E', this.player, this.board.height(), this.board.width());
-        
         this.objective = new Objective(new Vector2D(2, 2), player, '*');
-
         //Initial rendering
         renderBoard();
-    }
+    } // init()
+
     /**
      * Represent a single turn of the game
      */
@@ -53,40 +57,67 @@ public class Game {
         updateGameObjects();
         if (this.player.collidesWith(this.enemy)) {
             this.gameOver = true;
-        }
+        } // if
         renderBoard();
-    }
+    } // update()
 
+    /**
+     * Updates all the game objects
+     */
     private void updateGameObjects() { 
         for(GameObject go : this.gameObjects) {
             go.update();
-        }
-    }
+        } // for
+    } // updateGameObjects()
 
+    /**
+     * Make the board
+     */
     private void renderBoard() { 
         this.board.fillRegion(0, 0, this.board.height(), this.board.width(), '.');
         for(GameObject go : this.gameObjects) { 
             for(int i = 0; i < go.size.getX(); ++i) { 
                 for(int j = 0; j < go.size.getY(); ++j) { 
                     this.board.set(go.position.getY() + i, go.position.getX() + j, go.character);
-                }
-            }
-        }
-    }
+                } // for
+            } // for
+        } // for
+    } // renderBoard()
 
+    /**
+     * Check if game is over.
+     *
+     * @return true for a game that is over.
+     */
     public boolean getGameOver() {
         return this.gameOver;
-    }
+    } // getGameOver()
 
+    /**
+     * Get the board.
+     *
+     * @return the board.
+     */
     public Matrix<java.lang.Character> getBoard() { 
         return this.board;
-    }
+    } // getBoard()
 
+    /**
+     * Get the current score.
+     *
+     * @return the score.
+     */
     public int getScore() { 
         return this.score;
-    }
+    } // getScore()
 
+    /**
+     * Set the score.
+     *
+     * @param val
+     *      Value to set score to.
+     */
     public void setScore(int val) {
         this.score = val;
-    }
-}
+    } // setScore(int)
+} // class Game
